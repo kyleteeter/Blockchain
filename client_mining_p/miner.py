b80f0bb92a14a663c718c70c47acc8014d0cc731
unknown <kyleteeter12@gmail.com>
2019-08-07 21:31:00 -0500

    Trying to get last proof

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -6,6 +6,38 @@ import sys
 
 # TODO: Implement functionality to search for a proof 
 
+# Run the proof_of_work function until a valid proof is found, validating or rejecting each attempt
+
+def proof_of_work(self, last_proof):
+    """
+    Simple Proof of Work Algorithm
+    Find a number p such that hash(last_block_string, p) contains 6 leading
+    zeroes
+    """
+    proof = 0
+    while self.valid_proof(last_proof , proof) is False:
+        proof +=1
+
+    return proof
+
+# Request the latest proof from the last_proof endpoint on the server
+def valid_proof(last_proof, proof):
+    """
+    Validates the Proof:  Does hash(block_string, proof) contain 6
+    leading zeroes?
+    """
+    # TODO
+    #build string to hash
+    guess = f'{last_proof}{proof}'.encode()
+    # use hash function
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    #Check if 6 leading 0's
+    beg = guess_hash[0:6] #[:6]
+    if beg == "000000":
+        return True
+    else:
+        return False
+    pass
 
 if __name__ == '__main__':
     # What node are we interacting with?
@@ -18,6 +50,7 @@ if __name__ == '__main__':
     # Run forever until interrupted
     while True:
         # TODO: Get the last proof from the server and look for a new one
+        r = requests.get(url = node + "/last_proof")
         # TODO: When found, POST it to the server {"proof": new_proof}
         # TODO: We're going to have to research how to do a POST in Python
         # HINT: Research `requests` and remember we're sending our data as JSON
